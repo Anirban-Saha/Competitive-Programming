65156c47e320f4eee264341a3dd5668552b5efef
Anirban Saha <anirbansaha0101@gmail.com>
2022-05-14 22:01:31 +0530

    commit

--- Misc/.vscode/Solve The Parentheses.cpp
+++ Misc/.vscode/Solve The Parentheses.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
 using namespace std;
-int main() {
-int i, j,k=0,n=0;
-string s; char c; cin>>s;
-j = s.length();
-for(i=0;i<j;i++){
-if(j%2==0)
-cout<<j/2;
+int main() { string s; cin>>s;
+if(s.length()%2==0)
+cout<<s.length()/2;
 else
 cout<<0;
-return 0;
-}
-}
+return 0;}
