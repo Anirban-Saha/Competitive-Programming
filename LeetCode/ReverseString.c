b4cc79ede43cef61b5db09f88b0a71d1fcba6785
Anirban Saha <anirbansaha0101@gmail.com>
2022-06-06 23:24:37 +0530

    Reverse a String using function

---
+++ LeetCode/ReverseString.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+#define m 100
+char rev(char str[m]);
+int main(void)
+{
+    printf("\nEnter a string!\n");
+    char str[m];
+    gets(str);
+    printf("\nThe string after reversal is:");
+    rev(str);
+    
+}
+char rev(char str[m])
+{
+    char r[m];
+    int i, c=0;
+    int l=strlen(str);
+    for (i = l - 1; i >= 0; i--)
+  	{
+  		r[c++] = str[i];
+  	}
+    r[l]='\0';
+    printf("\n%s",r);
+    
+}
