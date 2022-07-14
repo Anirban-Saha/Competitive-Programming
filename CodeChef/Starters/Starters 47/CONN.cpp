f14e8e66ceb4727aaa73996e35a7d873fac43e22
Anirban Saha <anirbansaha0101@gmail.com>
2022-07-14 20:03:54 +0530

    .

---
+++ CodeChef/Starters/Starters 47/CONN.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+long long t;
+cin>>t;
+while(t--)
+{
+int n;
+    cin>>n;
+    if(n==1||n==3||n==5)
+    cout<<"NO"<<endl;
+    else{
+
+    cout<<"Yes"<<endl;
+    }
+}
+return 0;
+}
