50ff0c698a5810b97b1d17cbecf6f078c2ff600c
Anirban Saha <anirbansaha0101@gmail.com>
2022-07-24 22:19:19 +0530

    .

--- LeetCode/724.find-pivot-index.cpp
+++ LeetCode/724.find-pivot-index.cpp
@@ -9,7 +9,7 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
               vector<int> prefix(nums.size()), prefix2(nums.size());
-        int l=nums.size(); int sum;
+        int l=nums.size(); int sum,l;
         prefix[0]=nums[l-1];
         for(int i=1;i<l;i++){
             nums[i]=nums[i]+nums[i-1];
