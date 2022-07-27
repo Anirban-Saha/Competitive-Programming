b3eaa06aceb9f570811adc9cf772836e2c80ccb0
Anirban Saha <88977035+Anirban-Saha@users.noreply.github.com>
2022-07-28 02:23:59 +0530

    Time: 34 ms (66.96%), Space: 31.2 MB (54.28%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -2,21 +2,12 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         //prefix[] stores the prefix sum of nums[]
-        vector<int> prefix(nums.size());
-        int sum2=0;
         int l=nums.size();
-        //Prefix sum of nums in prefix: 
-        for(int i=0;i<l;i++){
-        sum2=sum2+nums[i];
-        prefix[i]=sum2;
-        }
-        //Total stores the total sum of the vector given 
-        int total=prefix[l-1];
+        int sum2=0, sum=0;
+        sum2=accumulate(nums.begin(), nums.end(), 0);
         for(int i=0; i<l;i++)
-        {   
-            // If sum till i-th index-the current value is equal to the total - sum till i-th index
-            // cout<<prefix[i]-nums[i]<<"<----> "<<total-prefix[i]<<"prefix--->"<<prefix[i]<<endl;
-            if((prefix[i]-nums[i])==(total-prefix[i]))
+        {   sum=sum+nums[i];
+            if((sum-nums[i])==(sum2-sum))
             {
                 return i;
             } 
