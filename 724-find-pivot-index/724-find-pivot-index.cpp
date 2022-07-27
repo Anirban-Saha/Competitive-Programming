0537e4bb5ca2c577053b0e7ade2981db34962313
Anirban Saha <88977035+Anirban-Saha@users.noreply.github.com>
2022-07-28 01:16:31 +0530

    Time: 32 ms (72.17%), Space: 32.6 MB (5.51%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -5,17 +5,13 @@ public:
         vector<int> prefix;
         int sum2=0;
         int l=nums.size();
-        //Prefix sum of nums in prefix: 
         for(int i=0;i<l;i++){
         sum2=sum2+nums[i];
         prefix.push_back(sum2);
         }
-        //Total stores the total sum of the vector given 
         int total=prefix[l-1];
         for(int i=0; i<l;i++)
         {   
-            // If sum till i-th index-the current value is equal to the total - sum till i-th index
-            // cout<<prefix[i]-nums[i]<<"<----> "<<total-prefix[i]<<"prefix--->"<<prefix[i]<<endl;
             if((prefix[i]-nums[i])==(total-prefix[i]))
             {
                 return i;
