aa2d5fe763d33ffe06cd672e4ce1be02b940cdff
Anirban Saha <88977035+Anirban-Saha@users.noreply.github.com>
2022-07-28 01:20:46 +0530

    Time: 54 ms (19.09%), Space: 32.6 MB (5.51%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         //prefix[] stores the prefix sum of nums[]
+        int l=nums.size();
         vector<int> prefix;
         int sum2=0;
-        int l=nums.size();
         for(int i=0;i<l;i++){
         sum2=sum2+nums[i];
         prefix.push_back(sum2);
